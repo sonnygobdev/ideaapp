92cf5b0b5b65cd2d8ecd8e2909bd6fe98fead449
Lysander Gob <lysander.gob@towerswatson.com>
2020-04-30 16:02:27 +0800

    Initial Commit

---
+++ IdeaApp.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using IdeaApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdeaApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
